f34c7dfadfd9ea193bd14d5c08e059981c7d5cbb
rahulmalu1998 <rahulmalu1998@gmail.com>
2021-08-06 02:58:51 +0530

    MVP

--- SpeechDownloader.py
+++ SpeechDownloader.py
@@ -19,163 +19,27 @@ import audioUtils
 # Google Speech Commands Dataset V2
 # ##################
 
-# GSCmdV2Categs = {'unknown' : 0, 'silence' : 1, '_unknown_' : 0,'_silence_' : 1, '_background_noise_' : 1, 'yes' : 2,
+# MVPCategs = {'unknown' : 0, 'silence' : 1, '_unknown_' : 0,'_silence_' : 1, '_background_noise_' : 1, 'yes' : 2,
 #                 'no' : 3, 'up' : 4, 'down' : 5, 'left' : 6, 'right' : 7, 'on' : 8, 'off' : 9, 'stop' : 10, 'go' : 11}
-# numGSCmdV2Categs = 12
+# numMVPCategs = 12
 
 # "Yes", "No", "Up", "Down", "Left", "Right", "On", "Off", "Stop", "Go", "Zero",
 # "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", and "Nine"
 
-GSCmdV2Categs = {
-    'unknown': 0,
+MVPCategs = {
     'silence': 0,
-    '_unknown_': 0,
-    '_silence_': 0,
-    '_background_noise_': 0,
-    'yes': 2,
-    'no': 3,
-    'up': 4,
-    'down': 5,
-    'left': 6,
-    'right': 7,
-    'on': 8,
-    'off': 9,
-    'stop': 10,
-    'go': 11,
-    'zero': 12,
-    'one': 13,
-    'two': 14,
-    'three': 15,
-    'four': 16,
-    'five': 17,
-    'six': 18,
-    'seven': 19,
-    'eight': 20,
-    'nine': 1}
-numGSCmdV2Categs = 21
+    'S1': 1,
+    'S2': 2,
+    'EC': 3,
+    'murmur':4
+    }
+numMVPCategs = 5
 
 
 def PrepareGoogleSpeechCmd(version=2, forceDownload=False, task='20cmd'):
     """
-    Prepares Google Speech commands dataset version 2 for use
-
-    tasks: 20cmd, 12cmd, leftright or 35word
-
-    Returns full path to training, validation and test file list and file categories
-    """
-    allowedTasks = ['12cmd', 'leftright', '35word', '20cmd']
-    if task not in allowedTasks:
-        raise Exception('Task must be one of: {}'.format(allowedTasks))
-
-    basePath = None
-    if version == 2:
-        _DownloadGoogleSpeechCmdV2(forceDownload)
-        basePath = 'sd_GSCmdV2'
-    elif version == 1:
-        _DownloadGoogleSpeechCmdV1(forceDownload)
-        basePath = 'sd_GSCmdV1'
-    else:
-        raise Exception('Version must be 1 or 2')
-
-    if task == '12cmd':
-        GSCmdV2Categs = {
-            'unknown': 0,
-            'silence': 1,
-            '_unknown_': 0,
-            '_silence_': 1,
-            '_background_noise_': 1,
-            'yes': 2,
-            'no': 3,
-            'up': 4,
-            'down': 5,
-            'left': 6,
-            'right': 7,
-            'on': 8,
-            'off': 9,
-            'stop': 10,
-            'go': 11}
-        numGSCmdV2Categs = 12
-    elif task == 'leftright':
-        GSCmdV2Categs = {
-            'unknown': 0,
-            'silence': 0,
-            '_unknown_': 0,
-            '_silence_': 0,
-            '_background_noise_': 0,
-            'left': 1,
-            'right': 2}
-        numGSCmdV2Categs = 3
-    elif task == '35word':
-        GSCmdV2Categs = {
-            'unknown': 0,
-            'silence': 0,
-            '_unknown_': 0,
-            '_silence_': 0,
-            '_background_noise_': 0,
-            'yes': 2,
-            'no': 3,
-            'up': 4,
-            'down': 5,
-            'left': 6,
-            'right': 7,
-            'on': 8,
-            'off': 9,
-            'stop': 10,
-            'go': 11,
-            'zero': 12,
-            'one': 13,
-            'two': 14,
-            'three': 15,
-            'four': 16,
-            'five': 17,
-            'six': 18,
-            'seven': 19,
-            'eight': 20,
-            'nine': 1,
-            'backward': 21,
-            'bed': 22,
-            'bird': 23,
-            'cat': 24,
-            'dog': 25,
-            'follow': 26,
-            'forward': 27,
-            'happy': 28,
-            'house': 29,
-            'learn': 30,
-            'marvin': 31,
-            'sheila': 32,
-            'tree': 33,
-            'visual': 34,
-            'wow': 35}
-        numGSCmdV2Categs = 36
-    elif task == '20cmd':
-        GSCmdV2Categs = {
-            'unknown': 0,
-            'silence': 0,
-            '_unknown_': 0,
-            '_silence_': 0,
-            '_background_noise_': 0,
-            'yes': 2,
-            'no': 3,
-            'up': 4,
-            'down': 5,
-            'left': 6,
-            'right': 7,
-            'on': 8,
-            'off': 9,
-            'stop': 10,
-            'go': 11,
-            'zero': 12,
-            'one': 13,
-            'two': 14,
-            'three': 15,
-            'four': 16,
-            'five': 17,
-            'six': 18,
-            'seven': 19,
-            'eight': 20,
-            'nine': 1}
-        numGSCmdV2Categs = 21
+    Prepares Google Speech commands dataset version 2 for used"""
+    basePath = 'content/drive/MyDrive/split-events'
 
     print('Converting test set WAVs to numpy files')
     audioUtils.WAV2Numpy(basePath + '/test/')
@@ -203,18 +67,18 @@ def PrepareGoogleSpeechCmd(version=2, forceDownload=False, task='20cmd'):
                          f for f in files if f.endswith('.wav.npy')]
 
     # get categories
-    testWAVlabels = [_getFileCategory(f, GSCmdV2Categs) for f in testWAVs]
-    valWAVlabels = [_getFileCategory(f, GSCmdV2Categs) for f in valWAVs]
-    trainWAVlabels = [_getFileCategory(f, GSCmdV2Categs) for f in trainWAVs]
-    testWAVREALlabels = [_getFileCategory(f, GSCmdV2Categs)
+    testWAVlabels = [_getFileCategory(f,MVPCategs) for f in testWAVs]
+    valWAVlabels = [_getFileCategory(f, MVPCategs) for f in valWAVs]
+    trainWAVlabels = [_getFileCategory(f, MVPCategs) for f in trainWAVs]
+    testWAVREALlabels = [_getFileCategory(f, MVPCategs)
                          for f in testWAVsREAL]
 
     # background noise should be used for validation as well
     backNoiseFiles = [trainWAVs[i] for i in range(len(trainWAVlabels))
-                      if trainWAVlabels[i] == GSCmdV2Categs['silence']]
-    backNoiseCats = [GSCmdV2Categs['silence']
+                      if trainWAVlabels[i] == MVPCategs['silence']]
+    backNoiseCats = [MVPCategs['silence']
                      for i in range(len(backNoiseFiles))]
-    if numGSCmdV2Categs == 12:
+    if numMVPCategs == 12:
         valWAVs += backNoiseFiles
         valWAVlabels += backNoiseCats
 
@@ -244,92 +108,11 @@ def PrepareGoogleSpeechCmd(version=2, forceDownload=False, task='20cmd'):
 
     print('Done preparing Google Speech commands dataset version {}'.format(version))
 
-    return gscInfo, numGSCmdV2Categs
-
+    return gscInfo, numMVPCategs
 
 def _getFileCategory(file, catDict):
     """
-    Receives a file with name sd_GSCmdV2/train/<cat>/<filename> and returns an integer that is catDict[cat]
+    Receives a file with name sd_MVP/train/<cat>/<filename> and returns an integer that is catDict[cat]
     """
     categ = os.path.basename(os.path.dirname(file))
     return catDict.get(categ, 0)
-
-
-def _DownloadGoogleSpeechCmdV2(forceDownload=False):
-    """
-    Downloads Google Speech commands dataset version 2
-    """
-    if os.path.isdir("sd_GSCmdV2/") and not forceDownload:
-        print('Google Speech commands dataset version 2 already exists. Skipping download.')
-    else:
-        if not os.path.exists("sd_GSCmdV2/"):
-            os.makedirs("sd_GSCmdV2/")
-        trainFiles = 'http://download.tensorflow.org/data/speech_commands_v0.02.tar.gz'
-        testFiles = 'http://download.tensorflow.org/data/speech_commands_test_set_v0.02.tar.gz'
-        _downloadFile(testFiles, 'sd_GSCmdV2/test.tar.gz')
-        _downloadFile(trainFiles, 'sd_GSCmdV2/train.tar.gz')
-
-    # extract files
-    if not os.path.isdir("sd_GSCmdV2/test/"):
-        _extractTar('sd_GSCmdV2/test.tar.gz', 'sd_GSCmdV2/test/')
-
-    if not os.path.isdir("sd_GSCmdV2/train/"):
-        _extractTar('sd_GSCmdV2/train.tar.gz', 'sd_GSCmdV2/train/')
-
-
-def _DownloadGoogleSpeechCmdV1(forceDownload=False):
-    """
-    Downloads Google Speech commands dataset version 1
-    """
-    if os.path.isdir("sd_GSCmdV1/") and not forceDownload:
-        print('Google Speech commands dataset version 1 already exists. Skipping download.')
-    else:
-        if not os.path.exists("sd_GSCmdV1/"):
-            os.makedirs("sd_GSCmdV1/")
-        trainFiles = 'http://download.tensorflow.org/data/speech_commands_v0.01.tar.gz'
-        testFiles = 'http://download.tensorflow.org/data/speech_commands_test_set_v0.01.tar.gz'
-        _downloadFile(testFiles, 'sd_GSCmdV1/test.tar.gz')
-        _downloadFile(trainFiles, 'sd_GSCmdV1/train.tar.gz')
-
-    # extract files
-    if not os.path.isdir("sd_GSCmdV1/test/"):
-        _extractTar('sd_GSCmdV1/test.tar.gz', 'sd_GSCmdV1/test/')
-
-    if not os.path.isdir("sd_GSCmdV1/train/"):
-        _extractTar('sd_GSCmdV1/train.tar.gz', 'sd_GSCmdV1/train/')
-
-##############
-# Utilities
-##############
-
-
-def _downloadFile(url, fName):
-    # Streaming, so we can iterate over the response.
-    r = requests.get(url, stream=True)
-
-    # Total size in bytes.
-    total_size = int(r.headers.get('content-length', 0))
-    block_size = 1024
-    wrote = 0
-    print('Downloading {} into {}'.format(url, fName))
-    with open(fName, 'wb') as f:
-        for data in tqdm(r.iter_content(block_size),
-                         total=math.ceil(total_size // block_size),
-                         unit='KB',
-                         unit_scale=True):
-            wrote = wrote + len(data)
-            f.write(data)
-    if total_size != 0 and wrote != total_size:
-        print("ERROR, something went wrong")
-
-
-def _extractTar(fname, folder):
-    print('Extracting {} into {}'.format(fname, folder))
-    if (fname.endswith("tar.gz")):
-        tar = tarfile.open(fname, "r:gz")
-        tar.extractall(path=folder)
-        tar.close()
-    elif (fname.endswith("tar")):
-        tar = tarfile.open(fname, "r:")
-        tar.extractall(path=folder)
-        tar.close()
